5857f1460bb5b3f220de63ec1521502db9127bac
unknown <contact.xsprod@gmail.com>
2021-07-05 20:25:30 +0200

    fix password yup

--- src/inputValidator/password.js
+++ src/inputValidator/password.js
@@ -1,7 +1,7 @@
 const Yup = require('yup')
 
 const password = Yup.object({
-    password: Yup.string().required('TEST Un mot de passe est requis')
+    password: Yup.string().required('Un mot de passe est requis')
       .matches(
         /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_/])([-+!*$@%_\w]{16,})$/,
         'Votre mot de passe doit contenir au moins 16 caractères, une minuscule, une majuscule et un symbole spécial'
