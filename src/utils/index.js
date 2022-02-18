186e73e877fd8a6b8914c2aeeea021fc490d3f6c
steph <contact.xsprod@gmail.com>
2022-02-18 10:31:00 +0100

    fix export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
-const { getEnumValues, getEnumSubTypeValues, getEnumSubTypes } = require('./enum')
+const { getEnumValues, getEnumSubTypeValues, getEnumSubTypes, getEnumSubTypeValuesByParent, getEnumSubTypeByParent } = require('./enum')
 const { validateStringRequestItems } = require('./utils')
 
 module.exports = {
   getEnumValues,
   getEnumSubTypeValues,
   getEnumSubTypes,
+  getEnumSubTypeValuesByParent,
+  getEnumSubTypeByParent,
   validateStringRequestItems
 }
