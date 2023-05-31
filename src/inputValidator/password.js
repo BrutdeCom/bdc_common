e3372a2c840a6f3dcfd7b93e019221ece5129d0f
Stéphane Vivier <contact.xsprod@gmail.com>
2023-05-31 15:58:03 +0200

    fx pwd

--- src/inputValidator/password.js
+++ src/inputValidator/password.js
@@ -3,7 +3,8 @@ const Yup = require('yup')
 const password = Yup.object({
     password: Yup.string()
       .matches(
-        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_/])([-+!*$@%_\w]{8,})$/,
+        // /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_/])([-+!*$@%_\w]{8,})$/,
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
         'Votre mot de passe doit contenir au moins 8 caractères, une minuscule, une majuscule, un chiffre et un symbole spécial'
       )
   })
--- src/regex/password.js
+++ src/regex/password.js
@@ -3,6 +3,7 @@
  * @type {regex}
  */
 
- const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!$%*+/@_-])([\w!$%*+@-]{8,})$/
+//  const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!$%*+/@_-])([\w!$%*+@-]{8,})$/
+ const PASSWORD_REGEX = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
 
  module.exports = PASSWORD_REGEX
